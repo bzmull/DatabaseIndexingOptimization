4037953500da4e4f047b09163974c037cd972d94
bzmull <bmuller94@gmail.com>
2019-11-18 11:00:35 -0500

    updated submission

--- submission_file.sql
+++ submission_file.sql
@@ -4,36 +4,36 @@ SET profiling_history_size = 0;
 SET profiling_history_size = 100; 
 SET profiling = 1;
 
--- a. Find count of all employees who earn at least 5000 more than their managers. On above example data, should be Warren.
-select count(*) A
+-- a. Find count of all employees who earn at least 50 more than their managers. On above example data, should be Warren.
+select count(*) PART_A
 from employee emp1, employee emp2
 where emp1.Manager = emp2.ID
-and emp1.Salary >= emp2.Salary + 5;  -- assuming salaries are in thousands. Replace 5 with 5000 otherwise
+and emp1.Salary >= emp2.Salary + 50;  
 
 -- part (a) indexed
 create index ind on employee(manager, ID, Salary);
-select count(*) A_INDEXED
+select count(*) PART_A_INDEXED
 from employee emp1, employee emp2
 where emp1.Manager = emp2.ID
-and emp1.salary >= emp2.salary + 5000;
+and emp1.salary >= emp2.salary + 50;
 drop index ind on employee;
 
 -- b. Find the average salary earned by people taking the course of prof1038
-select avg(salary) B
+select avg(salary) PART_B
 from course, employee
 where course.prof = 'prof1038'
 and course.EmpID = employee.ID;
 
 -- part (b) indexed
 create index ind on course(prof, empid);
-select avg(salary) B_INDEXED
+select avg(salary) PART_B_INDEXED
 from course, employee
 where course.prof = 'prof1038'
 and course.EmpID = employee.ID;
 drop index ind on course;
 
 -- c. For those departments in which more than one employee takes courses, find the average salary by department.
-select department, avg(salary) C
+select department, avg(salary) PART_C
 from employee as tab1
 join (select department dept
 	from course, employee
@@ -45,7 +45,7 @@ group by department;
 
 -- part (c) indexed
 create index ind on employee(ID, department, salary);
-select department, avg(salary) C_INDEXED
+select department, avg(salary) PART_C_INDEXED
 from employee as tab1
 join (select department dept
 	from course, employee
